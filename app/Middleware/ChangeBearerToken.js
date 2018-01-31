2771242650301279645be3d420153c44729f9eb8
Utwo <mihai.legat@gmail.com>
2018-02-01 01:26:27 +0200

    Add middleware to change from Token to Bearer header for jwt auth

---
+++ app/Middleware/ChangeBearerToken.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class ChangeBearerToken {
+  // small hack for auth with jwt even when Token is send instead of Bearer
+  async handle ({ request }, next) {
+    if(request.header('authorization')) {
+      request.headers().authorization = request.header('authorization').replace('Token', 'Bearer')
+    }
+    await next()
+  }
+}
+
+module.exports = ChangeBearerToken
