57bd0f082c25f45cd631308e3c1b830b7a870595
jaonoctus <joaodias@noctus.org>
2020-01-23 13:19:02 -0300

    :recycle: (test) Create a new user with Factory

--- test/functional/login.spec.js
+++ test/functional/login.spec.js
@@ -1,25 +1,22 @@
 'use strict'
 
 const { test, trait } = use('Test/Suite')('Login')
-const User = use('App/Models/User')
+const Factory = use('Factory')
 
 trait('Test/ApiClient')
 
 test('Should return a user with valid credentials', async ({ client }) => {
-  const user = await User.create({
-    username: 'johnjacob',
-    email: 'test@test.com',
-    password: '123123'
-  })
+  const user = await Factory.model('App/Models/User').create()
 
   const response = await client.post('/users/login').send({
     user: {
       email: user.email,
-      password: '123123'
+      password: 'secret'
     }
   }).end()
 
   response.assertStatus(200)
+
   response.assertJSONSubset({
     user: {
       email: user.email,
